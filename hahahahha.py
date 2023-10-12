49fef27d1c0278b4f2b7a2adba64facf20eb3d26
Inglan2 <0358381@schoolsnet.act.edu.au>
2023-10-12 21:08:31 +1100

    mwahahahahahahahahahaha

--- hahahahha.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the current folder path
-current_folder_path = os.getcwd()
-
-# Iterate over all subdirectories and print the path of each file
-for root, dirs, files in os.walk(current_folder_path):
-    for file in files:
-        print(os.path.join(root, file))
--- swsurfer/README.md
+++
@@ -1,3 +0,0 @@
-# Subway-Surfers-Lite
-The Basic Clone of Subway Surfers !
-Hope You'll enjoy playing at ( https://subwaylite.netlify.app )
